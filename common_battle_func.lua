622bb68dbbd0751fe8c0c13aa648f5d8923a46ee
mihai1rus <154679738+mihai1rus@users.noreply.github.com>
2025-07-24 05:32:15 +0300

    edit

--- common_battle_func.lua
+++ common_battle_func.lua
@@ -47,65 +47,56 @@ function Common_Battle_Activate(ai, goal, probabilities, acts, actAfter)
     end
 end
 
-
-function errorAct(f3_arg0, ai, f3_arg2)
-    ai:AddSubGoal(GOAL_COMMON_ErrorNotification, 5, 0)
+function errorAct(ai, goal, f3_arg2)
+    goal:AddSubGoal(GOAL_COMMON_ErrorNotification, 5, 0)
     GetWellSpace_Odds = 0
     return GetWellSpace_Odds
     
 end
 
 function Common_Battle_Activate_ForCommonNPC(ai, goal, probabilities, acts, actAfter)
-    local f4_local0 = {}
-    local f4_local1 = {}
-    local max = 0
-    local f4_local3 = 1
-    for f4_local4 = 1, 500, 1 do
-        if acts[f4_local4] ~= nil then
-            f4_local0[f4_local4] = acts[f4_local4]
+    local actionFuncs = {}
+    local actionWeights = {}
+    local totalWeight = 0
+    local actMax = 500
+    for i = 1, actMax do
+        if acts[i] then
+            actionFuncs[i] = acts[i]
         else
-            f4_local0[f4_local4] = REGIST_FUNC(ai, goal, errorAct)
+            actionFuncs[i] = REGIST_FUNC(ai, goal, errorAct)
         end
-        if probabilities[f4_local4] ~= nil then
-            if probabilities[f4_local4] < 0 then
-                f4_local1[f4_local4] = 0
-            else
-                f4_local1[f4_local4] = probabilities[f4_local4]
-                max = max + f4_local1[f4_local4]
-            end
-        else
-        end
+        actionWeights[i] = probabilities[i] or 0
+        totalWeight = totalWeight + actionWeights[i]
     end
-    local f4_local4 = 0
-    local f4_local5 = ai:DbgGetForceActIdx()
-    if 0 < f4_local5 and f4_local5 <= 500 then
-        f4_local4 = f4_local0[f4_local5]()
-        ai:DbgSetLastActIdx(f4_local5)
+
+    local actionReturnValue = 0
+    local forcedActionIndex = ai:DbgGetForceActIdx()
+    if forcedActionIndex > 0 and forcedActionIndex <= actMax then
+        actionReturnValue = actionFuncs[forcedActionIndex]()
+        ai:DbgSetLastActIdx(forcedActionIndex)
     else
-        local random = ai:GetRandam_Int(1, max)
-        local f4_local7 = 0
-        local f4_local8 = 1
-        for f4_local9 = 1, 500, 1 do
-            if f4_local1[f4_local9] ~= nil then
-                f4_local7 = f4_local7 + f4_local1[f4_local9]
-                if random <= f4_local7 then
-                    f4_local4 = f4_local0[f4_local9]()
-                    ai:DbgSetLastActIdx(f4_local9)
-                    f4_local9 = 500
-                else
+        local randomPick = ai:GetRandam_Int(1, totalWeight)
+        local cumulativeWeight = 0
+
+        for i = 1, actMax do
+            if actionWeights[i] ~= nil then
+                cumulativeWeight = cumulativeWeight + actionWeights[i]
+                if randomPick <= cumulativeWeight then
+                    actionReturnValue = actionFuncs[i]()
+                    ai:DbgSetLastActIdx(i)
+                    break
                 end
             end
         end
     end
-    local random = ai:GetRandam_Int(1, 100)
-    if f4_local4 == nil or actAfter == nil then
-        f4_local4 = 0
+
+    local actAfterChance = ai:GetRandam_Int(1, 100)
+    if actionReturnValue == nil or actAfter == nil then
+        actionReturnValue = 0
     end
-    if random <= f4_local4 then
+    if actAfterChance <= actionReturnValue then
         actAfter()
     end
-    
-
 end
 
 function ResetActPerArr_ForCommonNPC(probabilities)
